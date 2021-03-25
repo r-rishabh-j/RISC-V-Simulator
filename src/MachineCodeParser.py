b9b85e4681157f7b99103920655a13787292ed05
Rishabh Jain <62394291+r-rishabh-j@users.noreply.github.com>
2021-03-25 19:27:34 +0530

    Comments supported. NOTE- Comments on the same line with a command not supported. This feature is included just so that we could include code with the machine instructions.

--- src/MachineCodeParser.py
+++ src/MachineCodeParser.py
@@ -12,30 +12,29 @@ def parser(FileName):
     for line in instructions:
         line_number=line_number+1
         line=line.strip() # removing unnecessary whitespaces \t, \n etc
+        if line=='' or line[0]=='#': # ignores empty lines and commented lines. Comment in the same line with a command not supported!
+            continue
         #print(f"\'{line}\'")
         line=line.split() # splitting the string into PC and INST word
         # PC_INST[line[0]]=line[1] # this was tried to store PC and instructions as strings in the dict. May be required in the future.
         #print(line)
         if len(line)!=2: # line should contain exactly 2 items.
-            if len(line)==0: # empty line found, no issues
-                continue
-            else:
-                print(f"Line {line_number}: Invalid syntax")
-                sys.exit()
+            print(f"Line {line_number}: Invalid syntax")
+            sys.exit()
         # parser will treat numbers entered without 0x as decimal, and with 0x as hexadecimal
         try:
-            line[0]=int(line[0]) # interpretted as base 10
+            line[0]=int(line[0]) # interpreted as base 10
         except: # number entered is not decimal
             try:
-                line[0]=int(line[0],16) # interpretted as base 16
+                line[0]=int(line[0],16) # interpreted as base 16
             except: # str is neither decimal nor hex
                 print(f"Line {line_number}: Invalid program counter (PC)")
                 sys.exit()
         try:
-            line[1]=int(line[1]) # interpretted as base 10
+            line[1]=int(line[1]) # interpreted as base 10
         except:
             try:
-                line[1]=int(line[1], 16) # interpretted as base 16
+                line[1]=int(line[1], 16) # interpreted as base 16
             except:
                 print(f"Line {line_number}: Invalid instruction format")
                 sys.exit()
