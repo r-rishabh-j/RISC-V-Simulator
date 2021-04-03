e717b4e13083e0fb557c892b6ab9a60d5074801b
AYUSH VERMA <2019csb1147@iitrpr.ac.in>
2021-04-03 15:07:07 +0530

    Update ControlCircuit.py

--- src/ControlCircuit.py
+++ src/ControlCircuit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# generates control signals for the ALU and muxes
+# generates control signals for the ALU and muxes 
 MAX_SIGNED_NUM=0x7fffffff
 MIN_SIGNED_NUM=0x10000000
 MAX_UNSIGNED_NUM=0xffffffff
