937015e41ba2ac062e28fca000b8e6b7ffcaad2e
Rishabh Jain <62394291+r-rishabh-j@users.noreply.github.com>
2021-04-04 16:50:03 +0530

    control signal updated

--- src/ControlCircuit.py
+++ src/ControlCircuit.py
@@ -40,57 +40,57 @@ MAX_UNSIGNED_NUM=0xffffffff
 MIN_UNSIGNED_NUM=0x00000000
 
 class ControlModule:
-	def __init__(self):
-		self.opcode = 0
-		self.funct3 = 0
-		self.funct7 = 0
-		self.rd = 0
-		self.rs1 = 0
-		self.rs2 = 0
-		self.imm = 0
+    def __init__(self):
+        self.opcode = 0
+        self.funct3 = 0
+        self.funct7 = 0
+        self.rd = 0
+        self.rs1 = 0
+        self.rs2 = 0
+        self.imm = 0
 
-	def getOpcode(self):
-		return self.opcode
-	def getFunct3(self):
-		return self.funct3
-	def getFunct7(self):
-		return self.funct7
-	def getRd(self):
-		return self.rd
-	def getRs1(self):
-		return self.rs1
-	def getRs2(self):
-		return self.rs2
-	def getImm(self):
-		return self.imm
+    def getOpcode(self):
+        return self.opcode
+    def getFunct3(self):
+        return self.funct3
+    def getFunct7(self):
+        return self.funct7
+    def getRd(self):
+        return self.rd
+    def getRs1(self):
+        return self.rs1
+    def getRs2(self):
+        return self.rs2
+    def getImm(self):
+        return self.imm
 
-	def setOpcode(self, opcode):
-		self.opcode = opcode
-	def setFunct3(self, funct3):
-		self.funct3 = funct3
-	def setFunct7(self, funct7):
-		self.funct7 = funct7
-	def setRd(self, rd):
-		self.rd = rd
-	def setRs1(self, rs1):
-		self.rs1 = rs1
-	def setRs2(self, rs2):
-		self.rs2 = rs2
-	def setImm(self, imm):
-		self.imm = imm
+    def setOpcode(self, opcode):
+        self.opcode = opcode
+    def setFunct3(self, funct3):
+        self.funct3 = funct3
+    def setFunct7(self, funct7):
+        self.funct7 = funct7
+    def setRd(self, rd):
+        self.rd = rd
+    def setRs1(self, rs1):
+        self.rs1 = rs1
+    def setRs2(self, rs2):
+        self.rs2 = rs2
+    def setImm(self, imm):
+        self.imm = imm
 
 class DecodeModule:
 
     def __init__(self):
-		self.opcode = 0
-		self.funct3 = 0
-		self.funct7 = 0
-		self.rd = 0
-		self.rs1 = 0
-		self.rs2 = 0
-		self.imm = 0
+        self.opcode = 0
+        self.funct3 = 0
+        self.funct7 = 0
+        self.rd = 0
+        self.rs1 = 0
+        self.rs2 = 0
+        self.imm = 0
 
-    def decodeI(machine_code):
+    def decodeI(self, machine_code):
         inst_list = []
         machine_code = machine_code >> 7
         inst_list.append((machine_code & 0x1f));    #rd
@@ -102,8 +102,8 @@ class DecodeModule:
         inst_list.append((-(machine_code & 0x800) | (machine_code & 0x7ff)))    #imm(signed)
         #print(inst_list)
         return inst_list;
-    
-    def decodeS(machine_code):
+
+    def decodeS(self,machine_code):
         inst_list = []
         machine_code = machine_code >> 7
         temp1 = machine_code & 0x1f     #building the register in temp1
@@ -118,8 +118,8 @@ class DecodeModule:
         temp1 = temp1+machine_code
         inst_list.append((-(temp1 & 0x800) | (temp1 & 0x7ff)))    #imm(signed)
         return inst_list
-    
-    def decodeU(machine_code):
+
+    def decodeU(self,machine_code):
         inst_list = []
         machine_code = machine_code>> 7
         inst_list.append((machine_code & 0x1f))     #rd
@@ -127,7 +127,7 @@ class DecodeModule:
         inst_list.append((-(machine_code & 0x80000) | (machine_code & 0x7ffff)))    #upper-imm(signed)
         return inst_list
 
-    def decodeR(machine_code):
+    def decodeR(self,machine_code):
         inst_list = []
         machine_code = machine_code >> 7
         inst_list.append((machine_code& 0x1f));    #rd
@@ -141,9 +141,9 @@ class DecodeModule:
         inst_list.append((machine_code & 0x7f));    #funct7
         return inst_list
 
-    def decodeSB(machine_code):
+    def decodeSB(self, machine_code):
         inst_list = []
-        func3=machine_code & 0x000007000  
+        func3=machine_code & 0x000007000
         func3=func3>>12
         inst_list.append(func3) #funct3
         r1=machine_code & 0x000F8000
@@ -168,9 +168,9 @@ class DecodeModule:
         inst_list.append(im)
         return inst_list
 
-    def decodeUJ(machine_code):
+    def decodeUJ(self, machine_code):
         inst_list = []
-        rdd=machine_code & 0x00000F80 
+        rdd=machine_code & 0x00000F80
         rdd=rdd>>7
         inst_list.append(rdd)   #rd
         temp12=machine_code>>12
@@ -194,26 +194,26 @@ class DecodeModule:
         self.opcode = (machine_code& 0x7f)
         #print(opcode, temp)
         if self.opcode in I:
-            inst_list = decodeI(machine_code)
+            inst_list = self.decodeI(machine_code)
             self.rd = inst_list[0]
             self.funct3 = inst_list[1]
             self.rs1 = inst_list[2]
             self.imm = inst_list[3]
             #print(PC, "I type", opcode, funct3, rs1, rd, imm)
         elif self.opcode in S:
-            self.inst_list = decodeS(machine_code)
-            self.funct3 = inst_list[0]
-            self.rs1 = inst_list[1]
-            self.rs2 = inst_list[2]
-            self.imm = inst_list[3]
+            self.inst_list = self.decodeS(machine_code)
+            self.funct3 = self.inst_list[0]
+            self.rs1 = self.inst_list[1]
+            self.rs2 = self.inst_list[2]
+            self.imm = self.inst_list[3]
             #print(PC, "S type", opcode, funct3, rs1, rs2, imm)
         elif self.opcode in U:
-            inst_list = decodeU(machine_code)
+            inst_list = self.decodeU(machine_code)
             self.rd = inst_list[0]
             self.imm = inst_list[1]
             #print(PC, "U type", opcode, rd, imm)
         elif self.opcode in R:
-            inst_list = decodeR(machine_code)
+            inst_list = self.decodeR(machine_code)
             self.rd = inst_list[0]
             self.funct3 = inst_list[1]
             self.rs1 = inst_list[2]
@@ -221,18 +221,18 @@ class DecodeModule:
             self.funct7 = inst_list[4]
             #print(PC, "R type", opcode, funct3, funct7, rs1, rs2, rd)
         elif self.opcode in SB:
-            inst_list = decodeSB(machine_code)
+            inst_list = self.decodeSB(machine_code)
             self.funct3 = inst_list[0]
             self.rs1 = inst_list[1]
             self.rs2 = inst_list[2]
             self.imm = inst_list[3]
             #print(PC, "SB type", opcode, funct3, rs1, rs2, imm)
         elif self.opcode in UJ:
-            inst_list = decodeUJ(machine_code)
+            inst_list = self.decodeUJ(machine_code)
             self.rd = inst_list[0]
             self.imm = inst_list[1]
             #print(PC, "UJ type", opcode, rd, imm)
         else:
             raise Exception("Not an instruction")
 
-circuit = ControlModule()
+#circuit = ControlModule()
