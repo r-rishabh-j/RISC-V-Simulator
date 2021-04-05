c47dd712cb95fd638379821c8f5d18ee2e4d02d5
Rishabh Jain <62394291+r-rishabh-j@users.noreply.github.com>
2021-04-05 19:32:31 +0530

    alu basic code added

--- src/ALU.py
+++ src/ALU.py
@@ -4,3 +4,40 @@ MAX_SIGNED_NUM=0x7fffffff
 MIN_SIGNED_NUM=-0x80000000
 MAX_UNSIGNED_NUM=0xffffffff
 MIN_UNSIGNED_NUM=0x00000000
+
+class ArithmeticLogicUnit:
+    def __init__(self):
+        self.output32=0
+        self.outputBool=False # for branch instructions
+        self.input1=0
+        self.input2=0
+
+    def ALUexecute(self, control, inp1, inp2):
+
+    def add(self):
+
+    def subtract(self):
+
+    def multiply(self):
+
+    def division(self):
+
+    def remainder(self):
+
+    def rightShift(self):
+    
+    def rightShiftlogical(self):
+
+    def rightShiftarithmetic(self):
+
+    def leftShift(self):
+
+    def leftShift(self):
+    
+    def bitwiseAND(self):
+
+    def bitwiseXOR(self):
+
+    def bitwiseOR(self):
+
+    # comparision operation TBD
